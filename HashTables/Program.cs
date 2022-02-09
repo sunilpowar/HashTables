524f614ffb0c0e353ea6cd4330152a5261cfef43
sunilpowar <96655604+sunilpowar@users.noreply.github.com>
2022-02-10 00:18:23 +0530

    Update Program.cs

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -12,8 +12,36 @@ namespace HashTables
         {
             Console.WriteLine("Welcome to Hash Table Program");
 
-            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-            FrequencyOfWords.GetFrequencyOfWords(paragraph);
+            hash.Add("0", "paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "they");
+            hash.Add("10", "keep");
+            hash.Add("11", "putting");
+            hash.Add("12", "themselves");
+            hash.Add("13", "deliberately");
+            hash.Add("14", "into");
+            hash.Add("15", "paranoid");
+            hash.Add("16", "avoidable");
+            hash.Add("17", "situations");
+           
+             string output = hash.GetV("3");
+            int count = 0;
+            for (int i = 0; i < hash.Getsize(); i++)
+            {
+                string temp = hash.GetV($"{i}");
+                if (output == temp)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"frequency of {output} is : {count}");
             Console.ReadLine();
         }
     }
