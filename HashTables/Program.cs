bdb247308477d28f5ff482cbb3905700b4b6cc60
sunilpowar <96655604+sunilpowar@users.noreply.github.com>
2022-02-10 00:19:33 +0530

    Update Program.cs

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -11,8 +11,17 @@ namespace HashTables
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hash Table Program");
+            
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "Be");
+            hash.Add("2", "Or");
+            hash.Add("3", "Not");
+            hash.Add("4", "To");
+            hash.Add("5", "Be");
 
-            string paragraph = "To be or not to be or not";
+            Console.WriteLine(hash.GetV("5"));
+            Console.WriteLine(hash.GetV("0"));
             FrequencyOfWords.GetFrequencyOfWords(paragraph);
             Console.ReadLine();
         }
