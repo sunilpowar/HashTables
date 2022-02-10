743cc2784b9b3b6a91059f33054ae4257e34b168
sunilpowar <96655604+sunilpowar@users.noreply.github.com>
2022-02-10 07:48:40 +0530

    Update Program.cs

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -12,8 +12,32 @@ namespace HashTables
         {
             Console.WriteLine("Welcome to Hash Table Program");
 
-            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-            FrequencyOfWords.GetFrequencyOfWords(paragraph);
+           MyMapNode<string, string> hash = new MyMapNode<string, string>(20);
+            Console.WriteLine(hash.IsEmpty());
+            hash.Add("0", "paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "they");
+            hash.Add("10", "keep");
+            hash.Add("11", "putting");
+            hash.Add("12", "themselves");
+            hash.Add("13", "deliberately");
+            hash.Add("14", "into");
+            hash.Add("15", "paranoid");
+            hash.Add("16", "avoidable");
+            hash.Add("17", "situations");
+           
+            Console.WriteLine(hash.GetV("5").GetHashCode());
+            Console.WriteLine(hash.GetV("0").GetHashCode());
+            Console.WriteLine($"size is: {hash.Getsize()}");
+            hash.Remove("16");
+            Console.WriteLine($"size is: {hash.Getsize()}");
             Console.ReadLine();
         }
     }
